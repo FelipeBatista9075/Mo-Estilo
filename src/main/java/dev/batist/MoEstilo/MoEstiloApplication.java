45d973565ca278017004ca704897069fb0e90182
FelipeBatista9075 <felipebdias248@gmail.com>
2025-05-22 20:25:30 -0300

    first commit

---
+++ src/main/java/dev/batist/MoEstilo/MoEstiloApplication.java
@@ -0,0 +1,13 @@
+package dev.batist.MoEstilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoEstiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoEstiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/batist/MoEstilo/MoEstiloApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.batist.MoEstilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoEstiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
