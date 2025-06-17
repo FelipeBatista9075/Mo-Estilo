05e501d6bdf0e3da6bbffcd8e9352e43c4998999
FelipeBatista9075 <felipebdias248@gmail.com>
2025-06-16 22:56:51 -0300

    feat(db): add migrations for basket and basket_products tables

--- src/main/resources/db/migration/V2__create_table_basket.sql
+++ src/main/resources/db/migration/V2__create_table_basket.sql
@@ -1,7 +1,7 @@
-CREATE TABLE basket(
-    id VARCHAR(100) PRIMARY KEY NOT NULL,
-    client BIGSERIAL,
+CREATE TABLE basket (
+    id UUID PRIMARY KEY NOT NULL,
+    client BIGINT,
     total_price NUMERIC(10,2) NOT NULL,
     status VARCHAR(10) NOT NULL,
-       CHECK(status IN ('SOLD', 'OPEN'))
-)
+    CHECK(status IN ('SOLD', 'OPEN'))
+);
--- src/main/resources/db/migration/V3__create_basket_products.sql
+++ src/main/resources/db/migration/V3__create_basket_products.sql
@@ -1,7 +1,7 @@
 CREATE TABLE basket_products (
-    basket_entity_id VARCHAR(100) NOT NULL,
-    products_id BIGSERIAL NOT NULL,
+    basket_entity_id UUID NOT NULL,
+    products_id BIGINT NOT NULL,
     PRIMARY KEY (basket_entity_id, products_id),
-    CONSTRAINT fk_basket FOREIGN KEY (basket_entity_id) REFERENCES basket(id),
-    CONSTRAINT fk_products FOREIGN KEY (products_id) REFERENCES products(id)
+    FOREIGN KEY (basket_entity_id) REFERENCES basket(id),
+    FOREIGN KEY (products_id) REFERENCES products(id)
 );
